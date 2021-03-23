d1e3addd678cc43e834eb176b1c0524c3922a3b3
1672107_snhu <1672107_snhu@NV-SNHU21-RDS1.apporto.com>
2021-03-22 21:39:58 -0400

    Began work on Contact.java implementation

--- CS320Milestone/src/main/Contact.java
+++ CS320Milestone/src/main/Contact.java
@@ -2,4 +2,16 @@ package main;
 
 public class Contact {
 
+	private static final byte CONTACT_ID_LEN = 10;
+	private static final int CONTACT_PNUM_LENGTH = 10;
+	private static final byte CONTACT_FNAME_LEN = 10;
+	private static final byte CONTACT_LNAME_LEN = 10;
+	private static final byte CONTACT_ADDRESS_LEN = 30;
+	private static final String INIT = "Initial";
+	private static final String INIT_NUM = "0123456789";
+	private String contactID;
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String address;
 }
