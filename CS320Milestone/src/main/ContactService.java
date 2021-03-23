0fb5074808af80948cb39ef4eaeb1838bb3c4c23
1672107_snhu <1672107_snhu@NV-SNHU21-RDS1.apporto.com>
2021-03-23 01:00:49 -0400

    Additional ContactService stuff

--- CS320Milestone/src/main/ContactService.java
+++ CS320Milestone/src/main/ContactService.java
@@ -1,5 +1,25 @@
 package main;
 
-public class ContactService {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
+public class ContactService {
+	//I don't know why this is throwing this warning
+	private List<Contact> contactList = new Arraylist<>();
+	private String userID;
+	
+	{userID = UUID.randomUUID().toString().substring(
+			0, Math.min(toString().length(), 10));}
+	
+	private String newUserID() {
+		return userID = UUID.randomUUID().toString().substring(
+				0, Math.min(toString().length(), 10));
+				
+	}
+	
+	public void newContact() {
+		Contact c = new Contact(newUserID());
+		contactList.add(c);
+	}
 }
