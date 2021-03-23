a8590cf0888bccb2624c3912087ad6ec110961f7
1672107_snhu <1672107_snhu@NV-SNHU21-RDS1.apporto.com>
2021-03-23 02:06:05 -0400

    Re-approach this

--- CS320Milestone/src/test/ContactTest.java
+++ CS320Milestone/src/test/ContactTest.java
@@ -2,13 +2,19 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 
 class ContactTest {
 
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void contactTest() {
+		
 	}
 
 }
