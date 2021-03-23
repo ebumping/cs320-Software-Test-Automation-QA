15c86c8da798b1df4407539c1ddd22c31b97d79f
1672107_snhu <1672107_snhu@NV-SNHU21-RDS1.apporto.com>
2021-03-22 20:54:07 -0400

    Initialized CS320 Milestone
    Contact.java and ContactService.java added to package: main
    
    Respective tests added to package: test

---
+++ CS320Milestone/src/test/ContactTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ContactTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
