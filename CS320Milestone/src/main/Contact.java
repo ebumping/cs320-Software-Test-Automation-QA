9e8d7277763e81d2097241600c82862215ad865d
1672107_snhu <1672107_snhu@NV-SNHU21-RDS1.apporto.com>
2021-03-22 23:32:37 -0400

    innit

--- CS320Milestone/src/main/Contact.java
+++ CS320Milestone/src/main/Contact.java
@@ -7,7 +7,7 @@ public class Contact {
 	private static final byte CONTACT_FNAME_LEN = 10;
 	private static final byte CONTACT_LNAME_LEN = 10;
 	private static final byte CONTACT_ADDRESS_LEN = 30;
-	private static final String INIT = "Initial";
+	private static final String INIT = "Init";
 	private static final String INIT_NUM = "0123456789";
 	private String contactID;
 	private String firstName;
