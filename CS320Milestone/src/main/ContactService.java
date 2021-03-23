8a6b9db8fb1f2f4238878ef59f2e737e988efe1a
1672107_snhu <1672107_snhu@NV-SNHU21-RDS1.apporto.com>
2021-03-23 01:29:29 -0400

    Contact service rough draft

--- CS320Milestone/src/main/ContactService.java
+++ CS320Milestone/src/main/ContactService.java
@@ -22,4 +22,57 @@ public class ContactService {
 		Contact c = new Contact(newUserID());
 		contactList.add(c);
 	}
+	
+	public void newContact(String fname) {
+		Contact c = new Contact(newUserID(), fname);
+		contactList.add(c);
+	}
+	
+	public void newContact(String fname,String lname) {
+		Contact c = new Contact(newUserID(), fname, lname);
+		contactList.add(c);
+	}
+	
+	public void newContact(String fname,String lname, String phonenum) {
+		Contact c = new Contact(newUserID(), fname, lname, phonenum);
+		contactList.add(c);
+	}
+	
+	public void newContact(String fname,String lname, String phonenum, String address) {
+		Contact c = new Contact(newUserID(), fname, lname, phonenum, address);
+		contactList.add(c);
+	}
+	private Contact searchContact(String id) throws Exception{
+		int i = 0;
+		while (i < contactList.size()) {
+			if (id.equals(contactList.get(i).getContactID())) {
+				return contactList.get(i);
+			}
+			i++;
+		}
+		throw new Exception("Woops...Something went wrong.");
+	}
+	public void delContact(String id) throws Exception{
+		contactList.remove(searchContact(id));
+	}
+	
+	public void updateFirstName(String id, String fname) throws Exception{
+		searchContact(id).updateFirstName(fname);
+	}
+	
+	public void updateLastName(String id, String lname) throws Exception{
+		searchContact(id).updateLastName(lname);
+	}
+	
+	public void updatePhoneNum(String id, String pnum) throws Exception{
+		searchContact(id).updatePhoneNumber(pnum);
+	}
+	
+	public void updateAddress(String id, String address) throws Exception{
+		searchContact(id).updateAddress(address);
+	}
+	
+	protected List<Contact> getContactList(){
+		return contactList;
+	}
 }
